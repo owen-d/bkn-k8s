890060441c4a45d2ef0dc99b1cfed7353fabca67
owen-d <ow.diehl@gmail.com>
2017-09-12 11:05:15 -0400

    parameterizes lego email

--- lego/init.sh
+++ lego/init.sh
@@ -1,1 +1,9 @@
-helm install --name letsencrypt --values values.yaml --namespace kube-lego stable/kube-lego
+#!/bin/bash
+
+if [[ -z $LEGO_EMAIL ]]
+then
+    echo "requires LEGO_EMAIL"
+    exit 1
+fi
+
+helm install --name letsencrypt --values values.yaml --set config.LEGO_EMAIL=$LEGO_EMAIL --namespace kube-lego stable/kube-lego
